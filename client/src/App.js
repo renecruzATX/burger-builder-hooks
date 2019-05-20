669478b010728e3eef1749e9b69f6f05571a0fab
Rene Cruz <renecruzATX@outlook.com>
2019-05-20 15:00:22 -0500

    converted Checkout container to hooks

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ const app = props => {
   //renders routes that don't need authentication
   let routes = (
     <Switch>
-      <Route path="/login" render={()=> <Auth/>}/>
+      <Route path="/login" render={props=> <Auth {...props}/>}/>
       <Route path="/" exact component={BurgerBuilder}/>
       <Redirect to="/"/>
     </Switch>
@@ -37,10 +37,10 @@ const app = props => {
   if (props.isAuthenticated) {
     routes = (
       <Switch>
-        <Route path="/checkout" render={()=> <Checkout/>}/>
-        <Route path="/orders" render={()=> <Orders/>}/>
+        <Route path="/checkout" render={props=> <Checkout {...props}/>}/>
+        <Route path="/orders" render={props=> <Orders {...props}/>}/>
         <Route path="/logout" component={Logout}/>
-        <Route path="/login" render={()=> <Auth/>}/>
+        <Route path="/login" render={props=> <Auth {...props}/>}/>
         <Route path="/" exact component={BurgerBuilder}/>
         <Redirect to="/"/>
       </Switch>
